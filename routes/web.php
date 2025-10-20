33ab4e1bcef93de0992bee33bf71c263d9f041d8
frdiskandr <fariditb159@gmail.com>
2025-10-21 06:27:18 +0700

    final routes

--- routes/web.php
+++ routes/web.php
@@ -11,36 +11,30 @@ Route::get('/', function () {
 //! ebook route
 Route::middleware(['auth', 'verified'])->group(function () {
 
-    // Rute utama dashboard Ebook (READ: index)
+    // Rute utama dashboard Ebook
     Route::get('/ebooks', [EbookController::class, 'index'])->name('ebooks.index');
 
-    // Rute untuk membuat Ebook baru (Formulir)
+    // Rute untuk membuat Ebook baru
     Route::get('/ebooks/create', [EbookController::class, 'create'])->name('ebooks.create');
 
-    // Rute untuk menyimpan Ebook baru (POST)
+    // Rute untuk menyimpan Ebook baru
     Route::post('/ebooks', [EbookController::class, 'store'])->name('ebooks.store');
 
-    // Rute untuk melihat detail Ebook (viewer) (READ: show)
+    // Rute untuk melihat detail Ebook
     Route::get('/ebooks/{ebook}', [EbookController::class, 'show'])->name('ebooks.show');
 
     // Rute KHUSUS untuk streaming/display PDF secara aman
     Route::get('/ebooks/{ebook}/stream', [EbookController::class, 'stream'])->name('ebooks.stream');
 
-    // Rute untuk menghapus Ebook (DELETE: destroy)
+    // Rute untuk menghapus Ebook
     Route::delete('/ebooks/{ebook}', [EbookController::class, 'destroy'])->name('ebooks.destroy');
 
-    // Mengganti rute '/dashboard' default Breeze ke rute Ebook kita
-    // Ini memastikan user diarahkan ke daftar Ebook setelah login.
+    // Mengganti rute '/dashboard'
     Route::get('/dashboard', function () {
         return redirect()->route('ebooks.index');
     })->name('dashboard');
 
-    // ... Rute lain (profile, dll.) ...
 });
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
